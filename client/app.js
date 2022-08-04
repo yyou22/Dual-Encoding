5e4682c2f5229f57164ba15a5bbd16d51e29b650
vyou <vyou@umich.edu>
2022-08-04 18:40:48 -0400

    initial movement added

--- client/app.js
+++ client/app.js
@@ -4,15 +4,18 @@ var $ = require('jquery')
 var d3 = require('d3')
 
 var map_ = ['#f48382', '#f8bd61', '#ece137', '#c3c580', '#82a69a', '#80b2c5', '#8088c5', '#a380c5', '#c77bab', '#9a9494'];
+var perturb = ['None', '0.01', '0.02', '0.03'];
 
 // create dummy pie data for angle degrees
 var pie_data = {a: 50, b:50}
 
 var drawArc = d3.arc()
                 .innerRadius(0)
-                .outerRadius(9.6)
+                .outerRadius(6.6)
                 .startAngle(0.5)
                 .endAngle(0.5+Math.PI);
+
+$('#slider1').on('input', e => $('span').text(perturb[e.target.value]));
 
 $(document).ready(function() {
 
@@ -39,18 +42,19 @@ $(document).ready(function() {
         // The below example draws a simple "scatterplot"
 
         var s = d3.select('.canvas')
-            .selectAll('.dot')
+            .selectAll()
             .data(data)
             .enter()
             .append('svg')
             .append('g')
+            .attr('class', 'circle_group')
             .attr("transform", function(d) {
                 return "translate(" + x1(d.x)  + "," + y1(d.y)  + ")"
             });
 
         s.append('circle')
             .attr('class', 'dot')
-            .attr('r', 10)
+            .attr('r', 7)
             .style("fill", function(d) {
                     return map_[d.target];
                 })
@@ -61,5 +65,51 @@ $(document).ready(function() {
                     })
                     .attr('class', 'arc')
                     .attr('d', drawArc);
+    })
+
+    d3.select("#trans1").on("click", function() {
+
+        d3.csv('/data/data1.csv', function(d, i) {
+            // convert to numerical values
+            d.x = +d.xposp
+        	d.y = +d.yposp
+        	d.pred = +d.pred
+        	d.target = +d.target
+            d.idx = i
+
+            return d
+        }).then(function(data) {
+
+            d3.select('.canvas')
+                .selectAll('.circle_group')
+                .data(data)
+                .enter();
+
+            d3.select('.canvas')
+                .selectAll('.arc')
+                .data(data)
+                .enter();
+
+            //move svg groups
+            d3.select('.canvas')
+                .selectAll('.circle_group')
+                .transition()
+                .ease(d3.easeSin)
+                .duration(600)
+                .attr("transform", function(d) {
+                    return "translate(" + x1(d.x)  + "," + y1(d.y)  + ")";
+                });
+
+            d3.select('.canvas')
+                .selectAll('.arc')
+                .transition()
+                .ease(d3.easeSin)
+                .duration(600)
+                .style("fill", function(d) {
+                    return map_[d.pred];
+                });
+
+        })
+
     })
 })
